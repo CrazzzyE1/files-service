a2f2661dcb9730f0d9fa7d20c092e4eb79efcd2b
Litvak Ivan <litvakivan@gmail.com>
2025-06-29 12:58:50 +0300

    add first commit and base settings

---
+++ src/main/java/ru/litvak/files_service/FilesServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.litvak.files_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesServiceApplication.class, args);
+	}
+
+}
